b64760ac83846bab684fee1bcdb3239da22762e9
kodunaiff <kodunaiff@gmail.com>
2024-02-15 00:34:08 +0800

    sort imports and del comments

--- places/views.py
+++ places/views.py
@@ -1,9 +1,8 @@
-from django.core.serializers import json
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, JsonResponse
-from django.template import loader
-from .models import Place
 from django.urls import reverse
+
+from .models import Place
 
 
 def show_places(request):
