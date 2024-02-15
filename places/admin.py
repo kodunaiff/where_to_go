52da2c9e589c5ea8f124e5a8013e2f96a14dc122
kodunaiff <kodunaiff@gmail.com>
2024-02-15 19:46:08 +0800

    change variable

--- places/admin.py
+++ places/admin.py
@@ -14,12 +14,12 @@ class ImageInline(SortableTabularInline):
     fields = ['img', 'get_preview', 'number_pic']
     extra = 2
 
-    def get_preview(self, obj):
-        if not obj.img:
+    def get_preview(self, image):
+        if not image.img:
             return format_html('нет изображения')
         return format_html(
             '<img src="{}" style="max-width:{}px; max-height:{}px;">',
-            obj.img.url, MAX_WIDTH, MAX_HEIGHT
+            image.img.url, MAX_WIDTH, MAX_HEIGHT
         )
 
     get_preview.short_description = 'preview'
